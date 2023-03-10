ad0a81ca79758acb75486033012b833aa4bfa21a
SGENZO <hou1403383262@gmail.com>
2023-03-10 14:24:55 +0800

    hamiltonian & loss 修改

--- ferminet/hamiltonian.py
+++ ferminet/hamiltonian.py
@@ -25,29 +25,31 @@ from typing_extensions import Protocol
 from ferminet.cmplx import hamiltonian as cmplx_h
 
 
-class LocalEnergy(Protocol):
+class MomentLocalEnergy(Protocol):
 
-  def __call__(self, params: networks.ParamTree, key: chex.PRNGKey,
-               data: jnp.ndarray) -> jnp.ndarray:
-    """Returns the local energy of a Hamiltonian at a configuration.
+    def __call__(self, params: networks.ParamTree, key: chex.PRNGKey,
+                 data: jnp.ndarray) -> jnp.ndarray:
+        """Returns the local energy of a Hamiltonian at a configuration.
+    返回的是某时刻的local energy，需要提供mcmc采样的data，以及某个时刻(总时间离散的时间网格).
 
     Args:
       params: network parameters.
       key: JAX PRNG state.
       data: MCMC configuration to evaluate.
+      这里时间是放进data，还是单独提出来呢
     """
 
 
-class MakeLocalEnergy(Protocol):
+class MakeMomentLocalEnergy(Protocol):
 
-  def __call__(self,
-               f: networks.FermiNetLike,
-               atoms: jnp.ndarray,
-               charges: jnp.ndarray,
-               nspins: Sequence[int],
-               use_scan: bool = False,
-               **kwargs: Any) -> LocalEnergy:
-    """Builds the LocalEnergy function.
+    def __call__(self,
+                 f: networks.FermiNetLike,
+                 atoms: jnp.ndarray,
+                 charges: jnp.ndarray,
+                 nspins: Sequence[int],
+                 use_scan: bool = False,
+                 **kwargs: Any) -> MomentLocalEnergy:
+        """Builds the LocalEnergy function.
 
     Args:
       f: Callable which evaluates the sign and log of the magnitude of the
@@ -61,9 +63,9 @@ class MakeLocalEnergy(Protocol):
 
 
 def local_kinetic_energy(
-    f: networks.LogFermiNetLike,
-    use_scan: bool = False) -> networks.LogFermiNetLike:
-  r"""Creates a function to for the local kinetic energy, -1/2 \nabla^2 ln|f|.
+        f: networks.LogFermiNetLike,
+        use_scan: bool = False) -> networks.LogFermiNetLike:
+    """Creates a function to for the local kinetic energy, -1/2 \nabla^2 ln|f|.
 
   Args:
     f: Callable which evaluates the log of the magnitude of the wavefunction.
@@ -74,64 +76,67 @@ def local_kinetic_energy(
     -1/2f \nabla^2 f = -1/2 (\nabla^2 log|f| + (\nabla log|f|)^2).
   """
 
-  def _lapl_over_f(params, data):
-    n = data.shape[0]
-    eye = jnp.eye(n)
-    grad_f = jax.grad(f, argnums=1)
-    grad_f_closure = lambda x: grad_f(params, x)
-    primal, dgrad_f = jax.linearize(grad_f_closure, data)
+    def _lapl_over_f(params, data, moment):
+        n = data.shape[0]
+        eye = jnp.eye(n)
+        grad_f = jax.grad(f, argnums=1)
+        grad_f_closure = lambda x: grad_f(params, x, moment)
+        primal, dgrad_f = jax.linearize(grad_f_closure, data)
+        # 这里的f是LogFermiNetLike(Params, electrons, time) -> log magnitude of function f
+        # grad_f是对electrons求的，用closure完成对参数params和时刻moment的固定
 
-    if use_scan:
-      _, diagonal = lax.scan(
-          lambda i, _: (i + 1, dgrad_f(eye[i])[i]), 0, None, length=n)
-      result = -0.5 * jnp.sum(diagonal)
-    else:
-      result = -0.5 * lax.fori_loop(
-          0, n, lambda i, val: val + dgrad_f(eye[i])[i], 0.0)
-    return result - 0.5 * jnp.sum(primal ** 2)
+        if use_scan:
+            _, diagonal = lax.scan(
+                lambda i, _: (i + 1, dgrad_f(eye[i])[i]), 0, None, length=n)
+            result = -0.5 * jnp.sum(diagonal)
+        else:
+            result = -0.5 * lax.fori_loop(
+                0, n, lambda i, val: val + dgrad_f(eye[i])[i], 0.0)
+        return result - 0.5 * jnp.sum(primal ** 2)
 
-  return _lapl_over_f
+    return _lapl_over_f
+# 完成时刻t的增加
 
 
 def potential_electron_electron(r_ee: jnp.ndarray) -> jnp.ndarray:
-  """Returns the electron-electron potential.
+    """Returns the electron-electron potential.
 
   Args:
     r_ee: Shape (neletrons, nelectrons, :). r_ee[i,j,0] gives the distance
       between electrons i and j. Other elements in the final axes are not
       required.
   """
-  return jnp.sum(jnp.triu(1 / r_ee[..., 0], k=1))
+    return jnp.sum(jnp.triu(1 / r_ee[..., 0], k=1))
 
 
 def potential_electron_nuclear(charges: jnp.ndarray,
                                r_ae: jnp.ndarray) -> jnp.ndarray:
-  """Returns the electron-nuclearpotential.
+    """Returns the electron-nuclearpotential.
 
   Args:
     charges: Shape (natoms). Nuclear charges of the atoms.
     r_ae: Shape (nelectrons, natoms). r_ae[i, j] gives the distance between
       electron i and atom j.
   """
-  return -jnp.sum(charges / r_ae[..., 0])
+    return -jnp.sum(charges / r_ae[..., 0])
 
 
 def potential_nuclear_nuclear(charges: jnp.ndarray,
                               atoms: jnp.ndarray) -> jnp.ndarray:
-  """Returns the electron-nuclearpotential.
+    """Returns the electron-nuclearpotential.
 
   Args:
     charges: Shape (natoms). Nuclear charges of the atoms.
     atoms: Shape (natoms, ndim). Positions of the atoms.
   """
-  r_aa = jnp.linalg.norm(atoms[None, ...] - atoms[:, None], axis=-1)
-  return jnp.sum(
-      jnp.triu((charges[None, ...] * charges[..., None]) / r_aa, k=1))
+    r_aa = jnp.linalg.norm(atoms[None, ...] - atoms[:, None], axis=-1)
+    return jnp.sum(
+        jnp.triu((charges[None, ...] * charges[..., None]) / r_aa, k=1))
 
 
 def potential_energy(r_ae: jnp.ndarray, r_ee: jnp.ndarray, atoms: jnp.ndarray,
                      charges: jnp.ndarray) -> jnp.ndarray:
-  """Returns the potential energy for this electron configuration.
+    """Returns the potential energy for this electron configuration.
 
   Args:
     r_ae: Shape (nelectrons, natoms). r_ae[i, j] gives the distance between
@@ -142,9 +147,9 @@ def potential_energy(r_ae: jnp.ndarray, r_ee: jnp.ndarray, atoms: jnp.ndarray,
     atoms: Shape (natoms, ndim). Positions of the atoms.
     charges: Shape (natoms). Nuclear charges of the atoms.
   """
-  return (potential_electron_electron(r_ee) +
-          potential_electron_nuclear(charges, r_ae) +
-          potential_nuclear_nuclear(charges, atoms))
+    return (potential_electron_electron(r_ee) +
+            potential_electron_nuclear(charges, r_ae) +
+            potential_nuclear_nuclear(charges, atoms))
 
 
 def local_energy(f: networks.FermiNetLike,
@@ -153,8 +158,8 @@ def local_energy(f: networks.FermiNetLike,
                  nspins: Sequence[int],
                  use_scan: bool = False,
                  do_complex: bool = False,
-) -> LocalEnergy:
-  """Creates the function to evaluate the local energy.
+                 ) -> MomentLocalEnergy:
+    """Creates the function to evaluate the local energy.
 
   Args:
     f: Callable which returns the sign and log of the magnitude of the
@@ -163,34 +168,35 @@ def local_energy(f: networks.FermiNetLike,
     charges: Shape (natoms). Nuclear charges of the atoms.
     nspins: Number of particles of each spin.
     use_scan: Whether to use a `lax.scan` for computing the laplacian.
+    do_complex: 是否用复数形式
 
   Returns:
     Callable with signature e_l(params, key, data) which evaluates the local
     energy of the wavefunction given the parameters params, RNG state key,
     and a single MCMC configuration in data.
   """
-  del nspins
-  # log_abs_f = lambda *args, **kwargs: f(*args, **kwargs)[1]
-  # already take the value of the network
-  log_abs_f = f
-  if do_complex:
-    ke = cmplx_h.local_kinetic_energy(log_abs_f)
-  else:
-    ke = local_kinetic_energy(log_abs_f, use_scan=use_scan)
+    del nspins
+    # log_abs_f = lambda *args, **kwargs: f(*args, **kwargs)[1]
+    # already take the value of the network
+    log_abs_f = f
+    if do_complex:
+        ke = cmplx_h.local_kinetic_energy(log_abs_f)
+    else:
+        ke = local_kinetic_energy(log_abs_f, use_scan=use_scan)
 
-  def _e_l(params: networks.ParamTree, key: chex.PRNGKey,
-           data: jnp.ndarray) -> jnp.ndarray:
-    """Returns the total energy.
+    def _e_l(params: networks.ParamTree, key: chex.PRNGKey,
+             data: jnp.ndarray) -> jnp.ndarray:
+        """Returns the total energy.
 
     Args:
       params: network parameters.
       key: RNG state.
       data: MCMC configuration.
     """
-    del key  # unused
-    _, _, r_ae, r_ee = networks.construct_input_features(data, atoms)
-    potential = potential_energy(r_ae, r_ee, atoms, charges)
-    kinetic = ke(params, data)
-    return potential + kinetic
+        del key  # unused
+        _, _, r_ae, r_ee = networks.construct_input_features(data, atoms)
+        potential = potential_energy(r_ae, r_ee, atoms, charges)
+        kinetic = ke(params, data)
+        return potential + kinetic
 
-  return _e_l
+    return _e_l
--- ferminet/loss.py
+++ ferminet/loss.py
@@ -28,28 +28,28 @@ from typing_extensions import Protocol
 
 @chex.dataclass
 class AuxiliaryLossData:
-  """Auxiliary data returned by total_energy.
+    """Auxiliary data returned by total_energy.
 
   Attributes:
     variance: mean variance over batch, and over all devices if inside a pmap.
     local_energy: local energy for each MCMC configuration.
   """
-  variance: jnp.DeviceArray
-  local_energy: jnp.DeviceArray
+    variance: jnp.DeviceArray
+    local_energy: jnp.DeviceArray
 
 
 class LossFn(Protocol):
 
-  def __call__(
-      self,
-      params_trial: networks.ParamTree,
-      params_wave: networks.ParamTree,
-      key_trial: chex.PRNGKey,
-      key_wave: chex.PRNGKey,
-      data_trial: jnp.ndarray,
-      data_wave: jnp.ndarray,
-  ) -> Tuple[jnp.ndarray, AuxiliaryLossData]:
-    """描述弱形式下优化问题的目标，详见文件weak form。
+    def __call__(
+            self,
+            params_trial: networks.ParamTree,
+            params_wave: networks.ParamTree,
+            key_trial: chex.PRNGKey,
+            key_wave: chex.PRNGKey,
+            data_trial: jnp.ndarray,
+            data_wave: jnp.ndarray,
+    ) -> Tuple[jnp.ndarray, AuxiliaryLossData]:
+        """描述弱形式下优化问题的目标，详见文件weak form。
     这行不用看了 Evaluates the total energy of the network for a batch of configurations.
 
     Note: the signature of this function is fixed to match that expected by
@@ -70,6 +70,7 @@ class LossFn(Protocol):
       local energy per MCMC configuration. The loss and variance are averaged
       over the batch and over all devices inside a pmap.
     """
+# 这里的return是不是需要对trial和wave分别给一个返回值？
 
 
 def make_loss(network_trial: networks.LogFermiNetLike,
@@ -78,48 +79,64 @@ def make_loss(network_trial: networks.LogFermiNetLike,
               local_energy_wave: hamiltonian.LocalEnergy,
               clip_local_energy_trial: float = 0.0,
               clip_local_energy_wave: float = 0.0) -> LossFn:
-  """Creates the loss function, including custom gradients.
+    """Creates the loss function, including custom gradients.
 
   Args:
-    network: callable which evaluates the log of the magnitude of the
+    network_trial: callable which evaluates the log of the magnitude of the
+      trial function(square root of the log probability distribution) at a
+      single MCMC configuration given the network parameters.
+    network_wave: callable which evaluates the log of the magnitude of the
       wavefunction (square root of the log probability distribution) at a
       single MCMC configuration given the network parameters.
-    local_energy: callable which evaluates the local energy.
-    clip_local_energy: If greater than zero, clip local energies that are
+    local_energy_trial: callable which evaluates the local energy of the
+      trial function.
+    local_energy_wave: callable which evaluates the local energy of the
+      wavefunction.
+    clip_local_energy_trial: clip local energy for trial function.
+    clip_local_energy_wave: clip local energy for wavefunction.
+
+    For clip local energy. If greater than zero, clip local energies that are
       outside [E_L - n D, E_L + n D], where E_L is the mean local energy, n is
       this value and D the mean absolute deviation of the local energies from
       the mean, to the boundaries. The clipped local energies are only used to
       evaluate gradients.
 
   Returns:
-    Callable with signature (params, data) and returns (loss, aux_data), where
+    Callable with signature (params_t, param_w, data_t, data_w) and returns
+    (loss, aux_data), where
     loss is the mean energy, and aux_data is an AuxiliaryLossDataobject. The
     loss is averaged over the batch and over all devices inside a pmap.
   """
-  batch_local_energy_trial = jax.vmap(local_energy_trial, in_axes=(None, 0, 0), out_axes=0)
-  batch_local_energy_wave = jax.vmap(local_energy_wave, in_axes=(None, 0, 0), out_axes=0)
-  batch_network_trial = jax.vmap(network_trial, in_axes=(None, 0), out_axes=0)
-  batch_network_wave = jax.vmap(network_wave, in_axes=(None, 0), out_axes=0)
+    # local_energy的输入是(f: networks.FermiNetLike, atoms: jnp.ndarray, charges: jnp.ndarray). vmap
+    batch_local_energy_trial = jax.vmap(local_energy_trial, in_axes=(None, 0, 0), out_axes=0)
+    batch_local_energy_wave = jax.vmap(local_energy_wave, in_axes=(None, 0, 0), out_axes=0)
+    batch_network_trial = jax.vmap(network_trial, in_axes=(None, 0), out_axes=0)
+    batch_network_wave = jax.vmap(network_wave, in_axes=(None, 0), out_axes=0)
 
-  @jax.custom_jvp
-  def total_energy(
-      params_trial: networks.ParamTree,
-      params_wave: networks.ParamTree,
-      key_trial: chex.PRNGKey,
-      key_wave: chex.PRNGKey,
-      data_trial: jnp.ndarray,
-      data_wave: jnp.ndarray,
-  ) -> Tuple[jnp.ndarray, AuxiliaryLossData]:
-    """Evaluates the total energy of the network for a batch of configurations.
+    @jax.custom_jvp
+    def total_energy(
+            params_trial: networks.ParamTree,
+            params_wave: networks.ParamTree,
+            key_trial: chex.PRNGKey,
+            key_wave: chex.PRNGKey,
+            data_trial: jnp.ndarray,
+            data_wave: jnp.ndarray,
+    ) -> Tuple[jnp.ndarray, AuxiliaryLossData]:
+        """Evaluates the total energy of the network for a batch of configurations.
 
     Note: the signature of this function is fixed to match that expected by
     kfac_jax.optimizer.Optimizer with value_func_has_rng=True and
     value_func_has_aux=True.
 
     Args:
-      params: parameters to pass to the network.
-      key: PRNG state.
-      data: Batched MCMC configurations to pass to the local energy function.
+      params_trial: parameters to pass to the network of trial function.
+      params_wave: parameters to pass to the network of wave function.
+      key_trial: PRNG state.
+      key_wave: PRNG state.
+      data_trial: Batched MCMC configurations of the trial function to pass to
+        the local energy function.
+      data_wave: Batched MCMC configurations of the wavefunction to pass to
+        the local energy function.
 
     Returns:
       (loss, aux_data), where loss is the mean energy, and aux_data is an
@@ -127,46 +144,44 @@ def make_loss(network_trial: networks.LogFermiNetLike,
       local energy per MCMC configuration. The loss and variance are averaged
       over the batch and over all devices inside a pmap.
     """
-    keys_trial = jax.random.split(key_trial, num=data_trial.shape[0])
-    keys_wave = jax.random.split(key_wave, num=data_wave.shape[0])
-    e_l_trial = batch_local_energy_trial(params_trial, keys_trial, data_trial)
-    e_l_wave = batch_local_energy_wave(params_wave, keys_wave, data_wave)
+        keys_trial = jax.random.split(key_trial, num=data_trial.shape[0])
+        keys_wave = jax.random.split(key_wave, num=data_wave.shape[0])
+        e_l_trial = batch_local_energy_trial(params_trial, keys_trial, data_trial)
+        e_l_wave = batch_local_energy_wave(params_wave, keys_wave, data_wave)
 
-    E_pt_1 = constants.pmean(jnp.mean(e_l_tr))
+        E_pt_1 = constants.pmean(jnp.mean(e_l_tr))
 
+        loss = constants.pmean(jnp.mean(e_l))
+        variance = constants.pmean(jnp.mean((e_l - loss) ** 2))
+        return loss, AuxiliaryLossData(variance=variance, local_energy=e_l)
 
+    @total_energy.defjvp
+    def total_energy_jvp(primals, tangents):  # pylint: disable=unused-variable
+        """Custom Jacobian-vector product for unbiased local energy gradients."""
+        params, key, data = primals
+        loss, aux_data = total_energy(params, key, data)
 
-    loss = constants.pmean(jnp.mean(e_l))
-    variance = constants.pmean(jnp.mean((e_l - loss)**2))
-    return loss, AuxiliaryLossData(variance=variance, local_energy=e_l)
+        if clip_local_energy > 0.0:
+            # Try centering the window around the median instead of the mean?
+            tv = jnp.mean(jnp.abs(aux_data.local_energy - loss))
+            tv = constants.pmean(tv)
+            diff = jnp.clip(aux_data.local_energy,
+                            loss - clip_local_energy * tv,
+                            loss + clip_local_energy * tv) - loss
+        else:
+            diff = aux_data.local_energy - loss
 
-  @total_energy.defjvp
-  def total_energy_jvp(primals, tangents):  # pylint: disable=unused-variable
-    """Custom Jacobian-vector product for unbiased local energy gradients."""
-    params, key, data = primals
-    loss, aux_data = total_energy(params, key, data)
+        # Due to the simultaneous requirements of KFAC (calling convention must be
+        # (params, rng, data)) and Laplacian calculation (only want to take
+        # Laplacian wrt electron positions) we need to change up the calling
+        # convention between total_energy and batch_network
+        primals = primals[0], primals[2]
+        tangents = tangents[0], tangents[2]
+        psi_primal, psi_tangent = jax.jvp(batch_network, primals, tangents)
+        kfac_jax.register_normal_predictive_distribution(psi_primal[:, None])
+        primals_out = loss, aux_data
+        device_batch_size = jnp.shape(aux_data.local_energy)[0]
+        tangents_out = (jnp.dot(psi_tangent, diff) / device_batch_size, aux_data)
+        return primals_out, tangents_out
 
-    if clip_local_energy > 0.0:
-      # Try centering the window around the median instead of the mean?
-      tv = jnp.mean(jnp.abs(aux_data.local_energy - loss))
-      tv = constants.pmean(tv)
-      diff = jnp.clip(aux_data.local_energy,
-                      loss - clip_local_energy * tv,
-                      loss + clip_local_energy * tv) - loss
-    else:
-      diff = aux_data.local_energy - loss
-
-    # Due to the simultaneous requirements of KFAC (calling convention must be
-    # (params, rng, data)) and Laplacian calculation (only want to take
-    # Laplacian wrt electron positions) we need to change up the calling
-    # convention between total_energy and batch_network
-    primals = primals[0], primals[2]
-    tangents = tangents[0], tangents[2]
-    psi_primal, psi_tangent = jax.jvp(batch_network, primals, tangents)
-    kfac_jax.register_normal_predictive_distribution(psi_primal[:, None])
-    primals_out = loss, aux_data
-    device_batch_size = jnp.shape(aux_data.local_energy)[0]
-    tangents_out = (jnp.dot(psi_tangent, diff) / device_batch_size, aux_data)
-    return primals_out, tangents_out
-
-  return total_energy
+    return total_energy
